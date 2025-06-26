2a38c586eeecea9fff19e272fd1b1c5e7a6765f0
Hugodelima <hugorodrigues.contato@gmail.com>
2025-06-25 22:55:41 -0400

    fix: corrigido a porta do render.yaml

--- api-receita/src/main.ts
+++ api-receita/src/main.ts
@@ -1,10 +1,8 @@
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('Application is running on: http://localhost:3000');
+  await app.listen(process.env.PORT || 3000);  // Obriatório usar process.env.PORT!
 }
 bootstrap();
